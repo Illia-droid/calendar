dab860360bfd3379394701b961593ba870336e96
Illia <ilyachernykan@gmail.com>
2023-09-25 22:53:56 +0300

    Add imperative

--- src/components/Calendar/Month/index.jsx
+++ src/components/Calendar/Month/index.jsx
@@ -5,24 +5,36 @@ import { addWeeks, getWeeksInMonth, startOfMonth, startOfWeek } from "date-fns";
 import { DateContext } from "../../../contexts";
 
 const Month = () => {
-  const weeksArray = [];
   const { currentDate } = useContext(DateContext);
-  for (let index = 0; index <= getWeeksInMonth(currentDate); index++) {
-    weeksArray.push(
+  // const weeksArray = [];
+  // for (let index = 0; index <= getWeeksInMonth(currentDate); index++) {
+  //   weeksArray.push(
+  //     <Week
+  //       key={index}
+  //       currentDay={currentDate}
+  //       /* моя логіка полягає в тому, що я передаю перший день(в моєму випадку це понеділок)
+  //       першого тижня(понеділок може належати і до останнього тижня минулого місяця)
+
+  //       і за кожен цикл додається тиждень, тому я передаю в пропсах 'грубо кажучі' кожен понеділок*/
+  //       firstDayOfWeek={addWeeks(
+  //         startOfWeek(startOfMonth(currentDate), { weekStartsOn: 1 }),
+  //         index
+  //       )}
+  //     />
+  //   );
+  // }
+  const weeksArray = new Array(getWeeksInMonth(currentDate) + 1)
+    .fill(null)
+    .map((elem, i) => (
       <Week
-        key={index}
+        key={i}
         currentDay={currentDate}
-        /* моя логіка полягає в тому, що я передаю перший день(в моєму випадку це понеділок) 
-        першого тижня(понеділок може належати і до останнього тижня минулого місяця) 
-        
-        і за кожен цикл додається тиждень, тому я передаю в пропсах 'грубо кажучі' кожен понеділок*/
         firstDayOfWeek={addWeeks(
           startOfWeek(startOfMonth(currentDate), { weekStartsOn: 1 }),
-          index
+          i
         )}
       />
-    );
-  }
+    ));
   return <>{weeksArray}</>;
 };
 
--- src/components/Calendar/Week/index.jsx
+++ src/components/Calendar/Week/index.jsx
@@ -5,20 +5,29 @@ import { addDays, isSameMonth, isToday } from "date-fns";
 import styles from "./Week.module.scss";
 
 const Week = ({ currentDay, firstDayOfWeek }) => {
-  const daysArray = [];
-  for (let index = 0; index < 7; index++) {
-    /*тут я починаю заповнювати тижні днями, починаючи з того самого понеділка, 
-    котрий може належати до останнього тижня МИНУЛОГО місяця, та закінчую заповнення днями, які можуть належати
-    вже до НАСТУПНОГО місяца*/
-    let day = addDays(firstDayOfWeek, index);
+  // const daysArray = [];
+  // for (let index = 0; index < 7; index++) {
+  //   /*тут я починаю заповнювати тижні днями, починаючи з того самого понеділка,
+  //   котрий може належати до останнього тижня МИНУЛОГО місяця, та закінчую заповнення днями, які можуть належати
+  //   вже до НАСТУПНОГО місяца*/
+  //   let day = addDays(firstDayOfWeek, index);
+  //   const stylesForDay = cx(styles.day, { [styles.currentDay]: isToday(day) });
+  //   /* тут я перевіряю чи належить день до ПОТОЧНОГО місяця, і якщо true то пушу до масива*/
+  //   if (isSameMonth(day, currentDay)) {
+  //     daysArray.push(<Day key={index} day={day} classes={stylesForDay} />);
+  //   } else {
+  //     daysArray.push(<Day key={index} />);
+  //   }
+  // }
+  const daysArray = new Array(7).fill(null).map((el, i) => {
+    let day = addDays(firstDayOfWeek, i);
     const stylesForDay = cx(styles.day, { [styles.currentDay]: isToday(day) });
-    /* тут я перевіряю чи належить день до ПОТОЧНОГО місяця, і якщо true то пушу до масива*/
     if (isSameMonth(day, currentDay)) {
-      daysArray.push(<Day key={index} day={day} classes={stylesForDay} />);
+      return <Day key={i} day={day} classes={stylesForDay} />;
     } else {
-      daysArray.push(<Day key={index} />);
+      return <Day key={i} />;
     }
-  }
+  });
   return <tr>{daysArray}</tr>;
 };
 
